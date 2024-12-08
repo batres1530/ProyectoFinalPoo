e289dcb08bc09648b35a3dacd249b0efcfd95907
David Martinez <sergiodavidbatresmartinez@gmail.com>
2024-12-08 14:43:36 -0600

    se areglo el level one add gif and mokey, barril

--- Escenario.java
+++ Escenario.java
@@ -26,6 +26,7 @@ public class Escenario extends JPanel implements ActionListener, KeyListener {
     private Barril[] barriles; // Arreglo de barriles
     private Timer timerBarriles; // Temporizadores
     private int indiceBarrilActual = 0; // Índice actual para controlar dónde colocar el nuevo barril
+ 
 
     public Escenario(JFrame jfp) {
         icono = new ImageIcon("imagenes/fondo.png");
@@ -35,70 +36,82 @@ public class Escenario extends JPanel implements ActionListener, KeyListener {
         this.setVisible(true);
         this.frame = jfp;
         plataformas = new Estructura[85];
-        escaleras = new Escalera[10];
-        plataformas[0] = new Estructura(0, 640, "imagenes/Plataformasinicio.png");
-        plataformas[1] = new Estructura(0, 468, "imagenes/plataformade3.png");
-        plataformas[2] = new Estructura(64, 468, "imagenes/plataformade3.png");
-        plataformas[3] = new Estructura(128, 468, "imagenes/plataformade3.png");
-        plataformas[4] = new Estructura(192, 468, "imagenes/plataformade3.png");
-        plataformas[5] = new Estructura(256, 468, "imagenes/plataformade3.png");
-        plataformas[6] = new Estructura(320, 468, "imagenes/plataformade3.png");
-        plataformas[7] = new Estructura(384, 468, "imagenes/plataformade3.png");
-        plataformas[8] = new Estructura(448, 468, "imagenes/plataformade3.png");
-        plataformas[9] = new Estructura(512, 469, "imagenes/plataformade3.png");
-        plataformas[10] = new Estructura(576, 469, "imagenes/plataformade3.png");
-        plataformas[11] = new Estructura(640, 469, "imagenes/plataformade3.png");
-        plataformas[12] = new Estructura(704, 469, "imagenes/plataformade3.png");
-        plataformas[13] = new Estructura(768, 471, "imagenes/plataformade3.png");
-        plataformas[14] = new Estructura(832, 471, "imagenes/plataformade3.png");
-        plataformas[15] = new Estructura(896, 471, "imagenes/plataformade3.png");
-        plataformas[16] = new Estructura(960, 475, "imagenes/plataformade3.png");
-        plataformas[17] = new Estructura(1024, 477, "imagenes/plataformade3.png");
-        plataformas[18] = new Estructura(1119, 309, "imagenes/plataformade3.png");
-        plataformas[19] = new Estructura(1055, 309, "imagenes/plataformade3.png");
-        plataformas[20] = new Estructura(991, 309, "imagenes/plataformade3.png");
-        plataformas[21] = new Estructura(927, 309, "imagenes/plataformade3.png");
-        plataformas[22] = new Estructura(863, 309, "imagenes/plataformade3.png");
-        plataformas[23] = new Estructura(799, 309, "imagenes/plataformade3.png");
-        plataformas[24] = new Estructura(735, 309, "imagenes/plataformade3.png");
-        plataformas[25] = new Estructura(671, 309, "imagenes/plataformade3.png");
-        plataformas[26] = new Estructura(607, 310, "imagenes/plataformade3.png");
-        plataformas[27] = new Estructura(543, 310, "imagenes/plataformade3.png");
-        plataformas[28] = new Estructura(479, 311, "imagenes/plataformade3.png");
-        plataformas[29] = new Estructura(415, 311, "imagenes/plataformade3.png");
-        plataformas[30] = new Estructura(351, 311, "imagenes/plataformade3.png");
-        plataformas[31] = new Estructura(287, 312, "imagenes/plataformade3.png");
-        plataformas[32] = new Estructura(223, 312, "imagenes/plataformade3.png");
-        plataformas[33] = new Estructura(159, 313, "imagenes/plataformade3.png");
-        plataformas[34] = new Estructura(95, 314, "imagenes/plataformade3.png");
+        escaleras = new Escalera[15];
+        plataformas[0] = new Estructura(0, 655, "imagenes/Plataformasinicio.png");
+        
+        plataformas[1] = new Estructura(0, 479, "imagenes/plataformade3.png");
+        plataformas[2] = new Estructura(64, 479, "imagenes/plataformade3.png");
+        plataformas[3] = new Estructura(128, 479, "imagenes/plataformade3.png");
+        plataformas[4] = new Estructura(192, 479, "imagenes/plataformade3.png");
+        plataformas[5] = new Estructura(256, 479, "imagenes/plataformade3.png");
+        plataformas[6] = new Estructura(320, 479, "imagenes/plataformade3.png");
+        plataformas[7] = new Estructura(384, 479, "imagenes/plataformade3.png");
+        plataformas[8] = new Estructura(448, 479, "imagenes/plataformade3.png");
+        plataformas[9] = new Estructura(512, 480, "imagenes/plataformade3.png");
+        plataformas[10] = new Estructura(576, 480, "imagenes/plataformade3.png");;
+        plataformas[11] = new Estructura(640, 480, "imagenes/plataformade3.png");;
+        plataformas[12] = new Estructura(704, 480, "imagenes/plataformade3.png");;
+        plataformas[13] = new Estructura(768, 481, "imagenes/plataformade3.png");
+        plataformas[14] = new Estructura(832, 482, "imagenes/plataformade3.png");
+        plataformas[15] = new Estructura(896, 483, "imagenes/plataformade3.png");
+        plataformas[16] = new Estructura(960, 484, "imagenes/plataformade3.png");
+        plataformas[17] = new Estructura(1024, 485, "imagenes/plataformade3.png");
+        // segunda plataforma
+        plataformas[18] = new Estructura(1119, 312, "imagenes/plataformade3.png");
+        plataformas[19] = new Estructura(1055, 312, "imagenes/plataformade3.png");
+        plataformas[20] = new Estructura(991, 312, "imagenes/plataformade3.png");
+        plataformas[21] = new Estructura(927, 312, "imagenes/plataformade3.png");
+        plataformas[22] = new Estructura(863, 312, "imagenes/plataformade3.png");
+        plataformas[23] = new Estructura(799, 312, "imagenes/plataformade3.png");
+        plataformas[24] = new Estructura(735, 312, "imagenes/plataformade3.png");
+        plataformas[25] = new Estructura(671, 312, "imagenes/plataformade3.png");
+        plataformas[26] = new Estructura(607, 313, "imagenes/plataformade3.png");
+        plataformas[27] = new Estructura(543, 313, "imagenes/plataformade3.png");
+        plataformas[28] = new Estructura(479, 314, "imagenes/plataformade3.png");
+        plataformas[29] = new Estructura(415, 314, "imagenes/plataformade3.png");
+        plataformas[30] = new Estructura(351, 314, "imagenes/plataformade3.png");
+        plataformas[31] = new Estructura(287, 315, "imagenes/plataformade3.png");
+        plataformas[32] = new Estructura(223, 315, "imagenes/plataformade3.png");
+        plataformas[33] = new Estructura(159, 316, "imagenes/plataformade3.png");
+        plataformas[34] = new Estructura(95, 317, "imagenes/plataformade3.png");
+        // tercera princesa
         plataformas[35] = new Estructura(554, 65, "imagenes/plataformade3.png");
         plataformas[36] = new Estructura(490, 65, "imagenes/plataformade3.png");
         plataformas[37] = new Estructura(426, 65, "imagenes/plataformade3.png");
         plataformas[38] = new Estructura(362, 65, "imagenes/plataformade3.png");
-        plataformas[39] = new Estructura(0, 155, "imagenes/plataformade3.png");
-        plataformas[40] = new Estructura(64, 155, "imagenes/plataformade3.png");
-        plataformas[41] = new Estructura(128, 155, "imagenes/plataformade3.png");
-        plataformas[42] = new Estructura(192, 155, "imagenes/plataformade3.png");
-        plataformas[43] = new Estructura(256, 155, "imagenes/plataformade3.png");
-        plataformas[44] = new Estructura(320, 155, "imagenes/plataformade3.png");
-        plataformas[45] = new Estructura(384, 155, "imagenes/plataformade3.png");
-        plataformas[46] = new Estructura(448, 155, "imagenes/plataformade3.png");
-        plataformas[47] = new Estructura(512, 155, "imagenes/plataformade3.png");
-        plataformas[48] = new Estructura(576, 155, "imagenes/plataformade3.png");
-        plataformas[49] = new Estructura(640, 155, "imagenes/plataformade3.png");
-        plataformas[50] = new Estructura(704, 155, "imagenes/plataformade3.png");
-        plataformas[51] = new Estructura(768, 155, "imagenes/plataformade3.png");
-        plataformas[52] = new Estructura(832, 158, "imagenes/plataformade3.png");
-        plataformas[53] = new Estructura(896, 161, "imagenes/plataformade3.png");
-        plataformas[54] = new Estructura(960, 164, "imagenes/plataformade3.png");
-        plataformas[55] = new Estructura(1024, 167, "imagenes/plataformade3.png");
+        // mono
+        plataformas[39] = new Estructura(0, 160, "imagenes/plataformade3.png");
+        plataformas[40] = new Estructura(64, 160, "imagenes/plataformade3.png");
+        plataformas[41] = new Estructura(128, 160, "imagenes/plataformade3.png");
+        plataformas[42] = new Estructura(192, 160, "imagenes/plataformade3.png");
+        plataformas[43] = new Estructura(256, 160, "imagenes/plataformade3.png");
+        plataformas[44] = new Estructura(320, 160, "imagenes/plataformade3.png");
+        plataformas[45] = new Estructura(384, 160, "imagenes/plataformade3.png");
+        plataformas[46] = new Estructura(448, 160, "imagenes/plataformade3.png");
+        plataformas[47] = new Estructura(512, 160, "imagenes/plataformade3.png");
+        plataformas[48] = new Estructura(576, 160, "imagenes/plataformade3.png");
+        plataformas[49] = new Estructura(640, 160, "imagenes/plataformade3.png");
+        plataformas[50] = new Estructura(704, 160, "imagenes/plataformade3.png");
+        plataformas[51] = new Estructura(768, 160, "imagenes/plataformade3.png");
+        plataformas[52] = new Estructura(832, 161, "imagenes/plataformade3.png");
+        plataformas[53] = new Estructura(896, 162, "imagenes/plataformade3.png");
+        plataformas[54] = new Estructura(960, 163, "imagenes/plataformade3.png");
+        plataformas[55] = new Estructura(1024, 164, "imagenes/plataformade3.png");
         plataformas[56] = new Estructura(485, 0, "imagenes/princesa.png");
         plataformas[57] = new Estructura(40, 89, "imagenes/bariles.png");
-        escaleras[0] = new Escalera(900, 470, "imagenes/Escalera1.png");
-        escaleras[1] = new Escalera(200, 310, "imagenes/Escalera1.png");
+        plataformas[58] = new Estructura(0, 555, "imagenes/fuego.gif");
+        //escaleras
+        escaleras[0] = new Escalera(894, 485, "imagenes/Escalera1.png"); // 500 es la altura de la escala
+        escaleras[9] = new Escalera(894, 632, "imagenes/Escalera2.png");
+        escaleras[1] = new Escalera(223, 315, "imagenes/Escalera1.png");
+        escaleras[10] = new Escalera(223, 460, "imagenes/Escalera2.png");
         escaleras[2] = new Escalera(900, 160, "imagenes/Escalera1.png");
-        escaleras[3] = new Escalera(120, 60, "imagenes/Mono.png");
-
+        escaleras[3] = new Escalera(115, 85, "imagenes/Mono.gif");
+        escaleras[4] = new Escalera(298, 7, "imagenes/Escalera1.png");
+        escaleras[5] = new Escalera(243, 7, "imagenes/Escalera1.png");
+        escaleras[6] = new Escalera(553, 90, "imagenes/Escalera3.png"); // peincesa
+        escaleras[7] = new Escalera(553, 125, "imagenes/Escalera3.png"); // peincesa
+        escaleras[8] = new Escalera(553, 67, "imagenes/Escalera2.png"); // peincesa
         barriles = new Barril[MAX_BARRILES];
         for (Barril barril : barriles) {
             if (barril != null) {
@@ -258,6 +271,18 @@ public class Escenario extends JPanel implements ActionListener, KeyListener {
                 }
             }
         }
+
+        // for (Barril barril : barriles) {
+        //     if (barril != null) {
+        //         barril.mover(plataformas);
+        //         if (.getRectangle().intersects(barril.getRectangle())) {
+        //             barril.setVisible(false);
+        //             barril.setX(3000);
+        //         }
+        //     }
+        // }
+        
+
     
         repaint();
     }
